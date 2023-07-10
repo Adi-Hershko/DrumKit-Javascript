ded2ed70248b31072a0109cf212d456db67dc072
Adi Hershko <116923756+Adi-Hershko@users.noreply.github.com>
2023-07-10 22:37:32 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+function makeSound(key) 
+{
+    switch (key)
+    {
+        case "w":
+            new Audio("sounds/tom-1.mp3").play();
+        break;
+        case "a":
+            new Audio("sounds/tom-2.mp3").play();
+        break;
+        case "s":
+            new Audio("sounds/tom-3.mp3").play();
+        break;
+        case "d":
+            new Audio("sounds/tom-4.mp3").play();
+        break;
+        case "j":
+            new Audio("sounds/snare.mp3").play();
+        break;
+        case "k":
+            new Audio("sounds/crash.mp3").play();
+        break;
+        case "l":
+            new Audio("sounds/kick-bass.mp3").play();
+        break;
+        default:
+            console.log(this.innerHTML);
+    }
+}
+
+function buttonAnimation(currentKey)
+{
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function()
+    {
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
+
+var buttons = document.querySelectorAll("button");
+
+for (var i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener("click", function()
+    {
+        makeSound(this.innerHTML);
+        buttonAnimation(this.innerHTML);
+    });
+}
+
+document.addEventListener("keydown", function(event)
+{
+   makeSound(event.key);
+    buttonAnimation(event.key);
+});
